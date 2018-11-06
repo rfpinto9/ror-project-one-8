6e8b0aeaee086e28968dbd6bcc835e1416195e66
Ruben Pinto <rpinto@spartaglobal.com>
2018-11-06 16:02:49 +0000

    v.1.0 User authentication and nav bar added

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,6 +1,6 @@
 class DirectorsController < ApplicationController
   before_action :set_director, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
   # GET /directors
   # GET /directors.json
   def index
