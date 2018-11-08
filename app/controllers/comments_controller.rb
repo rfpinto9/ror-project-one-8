ba285dda895764c19e6df0ae4e4e82191484d1d9
Ruben Pinto <rpinto@spartaglobal.com>
2018-11-08 22:35:33 +0000

    V.5.0 - Added comments but not working; Added some more styling: pictures to movies and direct index, style the text and paragphs

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+    def create
+        @movie = Movie.find(params[:movie_id])
+        @comment = @movie.comments.create(comment_params)
+        redirect_to movie_path(@movie)
+    end
+
+    private def comment_params
+        params.require(:comment).permit(:body)
+    end
+end
