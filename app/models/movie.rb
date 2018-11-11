3a531d00c78c9ac51eaaafca24281017f1168277
Ruben Pinto <rpinto@spartaglobal.com>
2018-11-11 15:38:56 +0000

    v.7.2 - validationg added to movie rating to be between 1 and 10

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,5 @@ class Movie < ApplicationRecord
   belongs_to :director
   has_many :comments
   validates :title, uniqueness: true, presence: true
-
-
+  validates_numericality_of :rating, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 10
 end
